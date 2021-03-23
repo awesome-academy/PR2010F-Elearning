6977a2ed70845d9d08c7f92164d7e4731a3dde26
Jeremie <wucututa@gmail.com>
2021-03-23 14:21:19 +0700

    show-coursev3

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,17 @@
+class CoursesController < ApplicationController
+  before_action :find_course, only: [:show]
+  def index
+   @courses = Course.paginate(page: params[:page]).per_page(4)
+  end
+  def show
+   @course =  Course.find_by id: params[:id]
+   @questions = @course.questions
+  end
+  def find_course
+  @course =  Course.find_by id: params[:id]
+  unless @course.present?
+    flash[:success] = "Course doesn't exist"
+    redirect_to courses_url
+  end
+end
+end
